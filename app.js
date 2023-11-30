8c056e59ddf90013e8600776868e07c8e582aecf
prince-patel23 <prinspatel2605@gmail.com>
2023-11-30 15:57:38 +0530

    ejs and some data test

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("../wonderlust/models/listing.js");
+const path = require("path");
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -16,11 +18,17 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.send("hi, I am prince");
 });
 
+app.get("/listings",async (req,res) => {
+    let allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+});
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080!");

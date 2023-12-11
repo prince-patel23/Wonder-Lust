25befb9ff928eeeccb52973f9251bd484b240598
prince-patel23 <prinspatel2605@gmail.com>
2023-12-11 14:42:12 +0530

    Using prettier i formated my code

--- app.js
+++ app.js
@@ -6,81 +6,78 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-    .then(() => {
-        console.log("connected to MongoDB");
-    }).catch(err => {
-        console.log(err);
-    });
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
-
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-    res.send("hi, I am prince");
+  res.send("hi, I am prince");
 });
 
 //Index Route
-app.get("/listings",async (req,res) => {
-    let allListings = await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
 //New Route
-app.get("/listings/new",(req,res) => {
-    res.render("listings/new.ejs");
-})
-    
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
 //Show Route
-app.get("/listings/:id",async(req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{ listing});
-})
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
 
 //Create Route
-app.post("/listings", async(req,res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
 //Edit Route
-app.get("/listings/:id/edit",async(req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{ listing });
-
-})
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
 
 //Update Route
-app.put("/listings/:id",async(req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id ,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-})
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 
 //Delete Route
-app.delete("/listings/:id",async(req,res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-
-})
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 
 app.listen(8080, () => {
-    console.log("Server is running on port 8080!");
+  console.log("Server is running on port 8080!");
 });
--- models/listing.js
+++ models/listing.js
@@ -1,25 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   description: String,
   image: {
     type: String,
     set: function (v) {
-      if (typeof v === 'object' && v.url) {
+      if (typeof v === "object" && v.url) {
         return v.url;
       }
-      return v || "https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+      return (
+        v ||
+        "https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      );
     },
   },
   price: Number,
   location: String,
-  country: String
+  country: String,
 });
 
-const Listing = mongoose.model('Listing', listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;

54b359a1d68a46dfe6203e1e528e0da6841083b3
prince-patel23 <prinspatel2605@gmail.com>
2023-12-16 13:01:30 +0530

    error.ejs file added for custom errors handling

--- app.js
+++ app.js
@@ -110,7 +110,8 @@ app.use("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "somthing went wrong" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("listings/error.ejs", { message });
+  //  res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {

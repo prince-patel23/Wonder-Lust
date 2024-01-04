a6e07365045d55401b7c1335dd8df7c11b43caab
prince-patel23 <prinspatel2605@gmail.com>
2024-01-04 12:01:11 +0530

    Sesson related error solved

--- app.js
+++ app.js
@@ -58,7 +58,6 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 1000,
     httpOnly: true,
   },
